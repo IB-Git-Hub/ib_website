66ee4375b50a535f8d6abe4032327708e3e37362
IB-Git-Hub <IB.serraye@gmail.com>
2026-09-17 09:06:58 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const menuButton = document.querySelector(".menu-toggle");
+const nav = document.querySelector("#mainNav");
+
+menuButton.addEventListener("click", () => {
+  nav.classList.toggle("open");
+});
+
+document.querySelectorAll("#mainNav a").forEach(link => {
+  link.addEventListener("click", () => nav.classList.remove("open"));
+});
+
+document.querySelector("#year").textContent = new Date().getFullYear();
